38e83b6f6c90dda9d2e5783505037b4e53c41ddc
Anselmo Ortega <127802328+lfkanselmo@users.noreply.github.com>
2023-08-15 14:31:57 -0500

    taller de logica
    Taller momento 2

---
+++ TallerLogica/src/taller/Solucion.java
@@ -0,0 +1,13 @@
+/*
+ */
+package taller;
+
+/**
+ *
+ * @author Krausser
+ */
+public class Solucion {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
